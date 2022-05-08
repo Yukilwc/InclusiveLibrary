dd6600c4daa2a791ebf089a25ed574189de89400
Misaka <misaka1033@gmail.com>
2022-05-09 00:54:28 +0800

    u

---
+++ docs/.vuepress/components/Swiper/SingleSlide.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class=""></div>
+    <div class="" id="SingleSlide">
+      <div class="swiper-wrapper">
+        <!-- Slides -->
+        <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue";
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+onMounted(() => {
+    init()
+});
+const init = () => {
+  new Swiper("#SingleSlide", {});
+};
+</script>
+
+<style scoped lang="scss"></style>
--- docs/Frontend/Awesome-Design/Swiper/Swiper-Collection.md
+++ docs/Frontend/Awesome-Design/Swiper/Swiper-Collection.md
@@ -1,1 +1,5 @@
 # Swiper样例收录
+
+## 基础单Slide轮播
+
+<Swiper-SingleSlide></Swiper-SingleSlide>
