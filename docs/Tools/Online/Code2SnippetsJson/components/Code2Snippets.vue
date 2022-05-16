e45d7648b0737feaec19a0d8191b27eb9284d615
Misaka <misaka1033@gmail.com>
2022-05-16 13:56:54 +0800

    u

--- docs/Tools/Online/Code2SnippetsJson/components/Code2Snippets.vue
+++ docs/Tools/Online/Code2SnippetsJson/components/Code2Snippets.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>code 2</div>
+  <div>
+    <div class="">
+      <el-input type="textarea" v-model="code" :rows="12"></el-input>
+    </div>
+    <div class="mt10">
+      <el-button @click="convert">转换</el-button>
+    </div>
+    <div class="mt10">
+      <el-input type="textarea" v-model="jsonStr" :rows="12"></el-input>
+    </div>
+  </div>
 </template>
 
-<script lang='ts' setup>
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const code = ref("");
+const jsonStr = ref("");
+const convert = () => {
+  // 序列化处理引号等
+  //   let res = JSON.stringify(code.value);
+  let res = code.value;
+  // 处理$符号
+  res = res.replace(/\$/g, () => {
+    return "$$";
+  });
+  // 处理\n符号
+  let resList = res.split("\n");
+  let obj = {
+    body: resList,
+  };
+  // 打印结果
+  jsonStr.value = JSON.stringify(obj);
+};
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
