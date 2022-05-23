f687514c61a11e93f73de6a9bcb5859482b373bc
Misaka <misaka1033@gmail.com>
2022-05-23 18:03:56 +0800

    u

--- docs/Frontend/WebAssembly/Rust2Wasm/components/Rust2WasmDemo.vue
+++ docs/Frontend/WebAssembly/Rust2Wasm/components/Rust2WasmDemo.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>rust 2 wasm</div>
+    <div>rust 2 wasm demo</div>
+    <div class=''>
+        <el-button type="primary" @click="doCall">调用wasm方法</el-button>
+    </div>
 </template>
 
 <script lang='ts' setup>
+import init, { greet } from "./pkg/hello_wasm";
+
+const doCall = () => {
+    init()
+        .then(() => {
+            greet("WebAssembly")
+        });
+}
 </script>
 
 <style scoped lang="scss">
