523c4d57c48725619fdc2a70ff0ae294d6f1a2c2
Yukilwc <misaka1033@gmail.com>
2022-05-29 11:02:14 +0800

    u

---
+++ docs/.vuepress/components/Swiper/VueCarousel/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang='ts' setup>
+import { onMounted, ref } from "vue";
+const getImage = (index) => {
+  return new URL(`./images/${index}.png`, import.meta.url).href
+}
+const carouselRef = ref(null)
+</script>
+
+<style scoped lang="scss">
+
+</style>
